e357be04e2073fdefca56f8b749c0a5eccad242d
therealgenish <yoavg111@gmail.com>
2021-06-08 16:22:43 +0300

    Firebase initalization

---
+++ firebase_auth_client/lib/pages/authPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
